f812f1b15a17989afa1626184a217f025139b925
sebastianit <sebastianacuna.it@gmail.com>
2020-07-02 18:32:44 -0400

    fixed test

--- src/contentAction/contentAction.test.js
+++ src/contentAction/contentAction.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render } from '@testing-library/react'
+import { cleanup, fireEvent, render } from '@testing-library/react'
 import TestRenderer from 'react-test-renderer';
 import Icon from "../icons";
 import IconListModel from "../icons/models/icon-list.model";
@@ -8,29 +8,40 @@ import ContentAction from './index'
 
 describe('ContentActions tests', () => {
 
+  let componentContainter;
+  const onClick = jest.fn();
+
+  beforeEach(() => {
+
+    componentContainter = render(
+      <ContentAction label="Imprimir" onClick={onClick} icon="line-print">
+      </ContentAction>
+    );
+  })
+
+  afterEach(() => {
+    cleanup()
+  })
+
   test('should be rendered', () => {
-    const component = render( <ContentAction label="Imprimir" onClick={()=> console.log('acción')} icon="line-print"></ContentAction>);
-    expect(component).toBeTruthy();
+    expect(componentContainter).toBeTruthy();
   });
 
   test('should component contain label', () => {
 
-    const component = render( <ContentAction label="Imprimir" onClick={()=> console.log('acción')} icon="line-print"></ContentAction>);
-    expect(component.getByText(/Imprimir/)).toBeTruthy();
+    expect(componentContainter.getAllByText(/Imprimir/)).toBeTruthy();
 
   });
 
   test('should click event must call from container', () => {
-    const onClick = jest.fn();
-    const component = render( <ContentAction label="Imprimir" onClick={onClick} icon="line-print"></ContentAction>);
-    fireEvent.click(component.container.querySelector('div[data-testid="test-container-content-action"]'));
+    fireEvent.click(componentContainter.container.querySelector('div[data-testid="test-container-content-action"]'));
     expect(onClick).toBeCalled();
 
   });
 
   test('should be disabled', () => {
     const component = render( <ContentAction label="Imprimir" disabled onClick={()=> console.log('acción')} icon="line-print"></ContentAction>);
-    const element = component.getByTestId('test-container-content-action');
+    const element = component.container.querySelector('div[data-testid="test-container-content-action"]');
     expect(element.closest('div').className).toBe('contentAction disabled');
   });
 
