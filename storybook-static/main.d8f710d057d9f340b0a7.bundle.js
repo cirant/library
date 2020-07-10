0b7e7a9a2fc220ddef72f7219cd95f18ace4d98f
Pablo Cirant <pcirant.externo@coopeuch.cl>
2020-07-10 14:11:20 -0400

    removing storybook-static

--- storybook-static/main.d8f710d057d9f340b0a7.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1273:function(n,o,e){"use strict";e.r(o);var s=e(63),t=e(3);s.addons.setConfig({theme:t.themes.normal})},477:function(n,o,e){e(478),e(621),e(1273),e(1274),e(1384),e(1386),e(459),e(1422),n.exports=e(459)},540:function(n,o){}},[[477,1,2]]]);
