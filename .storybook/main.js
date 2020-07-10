4d07c2f7d9e1eae7b40ed8fcb9af9fee053f27c0
sebastianit <sebastianacuna.it@gmail.com>
2020-07-10 10:42:52 -0400

    [BUGFIX/STORYBOOKDEPLOY] fix bug in webpack for svg and font files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,33 @@
+
+const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
   addons: [
     '@storybook/addon-actions',
-    '@storybook/addon-links',
+    '@storybook/adgit don-links',
     '@storybook/addon-knobs',
     '@storybook/addon-docs',
     '@storybook/addon-viewport/register',
     '@storybook/addon-links/register'
   ],
+  webpackFinal: async (config, { configType }) => {
+    config.plugins.push(
+      new FileManagerPlugin({
+        onEnd: [
+          {
+            copy: [
+              { source: "./storybook-static/static/media/**", destination: "./storybook-static/static/css/static/media" }
+            ],
+            delete: [
+              './storybook-static/static/media',
+            ],
+          },
+        ]
+      })
+    );
+    return config;
+  },
+
 };
