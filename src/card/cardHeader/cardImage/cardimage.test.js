e24fc8218e5aeb0970e1cf0f56cd3b8f1bdb9a1c
sebastian2brains <67331667+sebastian2brains@users.noreply.github.com>
2020-07-18 21:23:24 -0400

    [DESIGN SYSTEM] add test to cardImage (#49)
    add test to cardImage

--- src/card/cardHeader/cardImage/cardimage.test.js
+++ src/card/cardHeader/cardImage/cardimage.test.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import Card from './index'
+import React from "react";
+import { render } from '@testing-library/react';
+import CardImage from './index'
 
-describe('Card Component', () => {
-  it('should be rendered', () => {
-    const component = render(<Card border> hola </Card>)
-    expect(component).toBeTruthy()
-  })
-})
+
+describe('CardImage Component behavior', () => {
+
+  test('should be rendered', () => {
+    const component = render(<CardImage imgUrl=''/>);
+    expect(component).toBeTruthy();
+    expect(component.container.querySelector('img')).toBeTruthy();
+  });
+
+  test('should component contain the img url and alt propers texts', () => {
+    const component = render(<CardImage imgUrl='image.png' alt='description for image'/>);
+    expect(component.container.querySelector("img[src='image.png']")).toBeTruthy()
+    expect(component.container.querySelector("img[alt='description for image']")).toBeTruthy()
+
+  });
+
+});
