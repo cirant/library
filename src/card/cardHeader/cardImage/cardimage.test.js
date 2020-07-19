4dccbe302e10a538c8c32d26d9825358449928b7
sebastianit <sebastianacuna.it@gmail.com>
2020-07-18 21:12:10 -0400

    [DESIGN SYSTEM] add correct test

--- src/card/cardHeader/cardImage/cardimage.test.js
+++ src/card/cardHeader/cardImage/cardimage.test.js
@@ -8,11 +8,14 @@ describe('CardImage Component behavior', () => {
   test('should be rendered', () => {
     const component = render(<CardImage imgUrl=''/>);
     expect(component).toBeTruthy();
+    expect(component.container.querySelector('img')).toBeTruthy();
   });
 
-  test('should component contain img', () => {
-    const component = render(<CardImage imgUrl=''/>);
-    expect(component.container.querySelector('img')).toBeTruthy();
+  test('should component contain the img url and alt propers texts', () => {
+    const component = render(<CardImage imgUrl='image.png' alt='description for image'/>);
+    expect(component.container.querySelector("img[src='image.png']")).toBeTruthy()
+    expect(component.container.querySelector("img[alt='description for image']")).toBeTruthy()
+
   });
 
 });
